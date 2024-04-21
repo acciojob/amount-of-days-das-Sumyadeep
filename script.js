2eb73cd574fd3b25482fd44d3222a2cbcca35772
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2024-04-21 12:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function daysOfAyear(){
+
+	if(n % 4 == 0) return 366;
+	else if(n % 100 != 0) return 366;
+	else if(n % 400 == 0) return 366;
+	else{
+		return 365;
+	}
+}
+
+daysOfAyear(2024);
